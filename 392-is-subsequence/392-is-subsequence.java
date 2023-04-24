350ef303b652467a6e865144e93b3083204d660a
Curiosity Rover <62722431+curiosityr0ver@users.noreply.github.com>
2023-04-24 18:00:50 +0530

    Time: 12 ms (6.64%), Space: 44.1 MB (5.54%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        if(s.equals(t)) return true;
+        if(t.length() == 0 && s.length() != 0) return false;
+        if(s.length() == 0 && t.length() != 0) return true;
+        
+        if(s.charAt(0) == t.charAt(0)) {
+            return isSubsequence(s.substring(1), t.substring(1));
+        } else {
+            return isSubsequence(s, t.substring(1));
+        }
+    }
+}
