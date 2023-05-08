5d24574dc30c2c0dfe054cb807e908addb814f2c
Curiosity Rover <62722431+curiosityr0ver@users.noreply.github.com>
2023-05-08 19:19:05 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (58.47%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -3,8 +3,7 @@ class Solution {
         int n = mat.length;
         int sum = 0;
         for(int i = 0; i < n; i++) {
-            sum += mat[i][i];
-            sum += mat[i][n-i-1];
+            sum += mat[i][i] + mat[i][n-i-1];
         }
         
         if(n%2 == 1) sum -= mat[n/2][n/2];
