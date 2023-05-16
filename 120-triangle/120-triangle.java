400a94c56307cd5795d5ec4a309fed5ea96044e9
Curiosity Rover <62722431+curiosityr0ver@users.noreply.github.com>
2023-05-17 00:41:03 +0530

    Time: 24 ms (5.12%), Space: 54.5 MB (5.76%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,23 +1,10 @@
 class Solution {
     int [][] dp = new int[1000][1000];
     boolean [][] visited = new boolean[1000][1000];
-    private Integer[][] memo;
 
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        memo = new Integer[n][n];
         return help(0, 0, triangle);
     }
-
-//     private int help(int row, int col, List<List<Integer>> arr) {
-//         if (memo[row][col] != null) return memo[row][col];
-
-//         memo[row][col] = arr.get(row).get(col);
-//         if (row < arr.size() - 1) 
-//             memo[row][col] += Math.min(help(row + 1, col, arr), help(row + 1, col + 1, arr));
-
-//         return memo[row][col];
-//     }
     
     int help( int row, int col, List<List<Integer>> arr) {
         if(visited[row][col]) return dp[row][col];
