f6674ee1b9be814f8cf295a08a79d20cf4eaa601
Curiosity Rover <62722431+curiosityr0ver@users.noreply.github.com>
2023-05-23 00:37:34 +0530

    Time: 102 ms (5.09%), Space: 45.6 MB (28.11%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -0,0 +1,61 @@
+class Solution {
+    Map<Integer, Integer> hm = new TreeMap<>();
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        for(int ele: nums) {
+            hm.put(ele, 1+ hm.getOrDefault(ele, 0));
+        }
+        
+        int [] arr = new int [hm.size()];
+        int ind = 0;
+        for(int key: hm.keySet()) {
+            arr[ind++] = key;
+            System.out.print(arr[ind-1] + ", ");
+        }
+        System.out.println();
+        int[] res = new int[k];
+        quickSort(arr, 0, arr.length-1);
+        for(int i = 0; i < k; i++) {
+            res[i] = arr[i];
+        }
+        
+        return res;
+    }
+    
+    
+    int partition(int array[], int low, int high) {
+
+    int pivot = array[high];
+    
+    int i = (low - 1);
+
+    for (int j = low; j < high; j++) {
+      if (hm.get(array[j]) >= hm.get(pivot)) {
+
+        i++;
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+
+    }
+
+    int temp = array[i + 1];
+    array[i + 1] = array[high];
+    array[high] = temp;
+
+    return (i + 1);
+  }
+     void quickSort(int array[], int low, int high) {
+    if (low < high) {
+
+      int pi = partition(array, low, high);
+      quickSort(array, low, pi - 1);
+      quickSort(array, pi + 1, high);
+    }
+  }
+               
+               
+               
+}
