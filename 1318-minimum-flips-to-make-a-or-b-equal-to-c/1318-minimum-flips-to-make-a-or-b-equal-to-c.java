140ba3ccd28659bb9fc260ac152bc5b114af1b50
Curiosity Rover <62722431+curiosityr0ver@users.noreply.github.com>
2023-06-10 00:26:32 +0530

    Time: 12 ms (8.68%), Space: 40.4 MB (5.78%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,13 +1,13 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
-        List<Integer> a1 = getBin(a);
-        List<Integer> b1 = getBin(b);
-        List<Integer> c1 = getBin(c);
-        int len = Math.max(a1.size(), Math.max(b1.size(), c1.size()));
+//         List<Integer> a1 = getBin(a);
+//         List<Integer> b1 = getBin(b);
+//         List<Integer> c1 = getBin(c);
+//         int len = Math.max(a1.size(), Math.max(b1.size(), c1.size()));
 
-        while(a1.size() < len) a1.add(0);
-        while(b1.size() < len) b1.add(0);
-        while(c1.size() < len) c1.add(0);
+//         while(a1.size() < len) a1.add(0);
+//         while(b1.size() < len) b1.add(0);
+//         while(c1.size() < len) c1.add(0);
 
         int res = 0;
 //         for(int i = 0; i < len; i++) {
@@ -19,15 +19,24 @@ class Solution {
 //                if(a1.get(i) + b1.get(i) == 0) res++;
 //            }
 //         }
-        for(int i = 0; i < len; i++) {
+        int x, y, z;
+        do {
+            x = a%2;
+            y = b%2;
+            z = c%2;
+            a /= 2;
+            b /= 2;
+            c /= 2;
             
-           if(c1.get(i) == 0) {
-               res += a1.get(i) + b1.get(i);
+            System.out.println(x + " or " + y + " = " + z);
+            
+           if(z == 0) {
+               res += x + y;
                
            } else {
-               if(a1.get(i) + b1.get(i) == 0) res++;
+               if(x + y == 0) res++;
            }
-        }
+        } while(a+b+c > 0);
 
         return res;
     }
