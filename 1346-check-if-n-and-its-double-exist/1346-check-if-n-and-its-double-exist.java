a80ea61c606f3657b5c1c9e4bad4c310ceeb37de
Curiosity Rover <62722431+curiosityr0ver@users.noreply.github.com>
2023-06-14 01:11:52 +0530

    Time: 2 ms (47.71%), Space: 41.6 MB (92.24%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Arrays.sort(arr);
+        
+        for(int i = 0; i < arr.length; i++) {
+            int res = rec(0, arr.length-1, arr, arr[i]*2);
+            if(res != i && res != -1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+    public int rec(int left, int right, int [] nums, int tag){
+        if(left>right) return -1;
+        
+        int mid = (left+right)/2;
+        
+        if(nums[mid] == tag)return mid;
+        else if(nums[mid] > tag) return rec(left, mid-1, nums, tag);
+        else return rec(mid+1, right, nums, tag);
+    }
+}
