03540285eb8614aa4716b22b8a9ee7cfd3012e7c
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-07-10 03:14:13 +0530

    Time: 12 ms (13.24%), Space: 41.3 MB (9.36%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -5,19 +5,8 @@ class Solution {
         for(int i = 0; i < s.length(); i++) {
             hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
         }
-        
-        for(char key: hm.keySet()) {
-            System.out.println(key + ":" + hm.get(key));
-        }
-        
         for(int i = 0; i < t.length(); i++) {
             hm.put(t.charAt(i), hm.getOrDefault(t.charAt(i), 0) - 1);
-        }
-        
-        
-        System.out.println("****************");
-        for(char key: hm.keySet()) {
-            System.out.println(key + ":" + hm.get(key));
         }
         
         for(char key: hm.keySet()) {
