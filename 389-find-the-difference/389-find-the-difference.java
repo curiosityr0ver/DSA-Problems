2a26042219d41bda1aa1e14423599ba8889b7f7f
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-07-10 03:20:09 +0530

    Time: 3 ms (43.37%), Space: 40.8 MB (85.54%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,17 +1,24 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         HashMap<Character, Integer> hm = new HashMap<>();
+        int[] dict = new int[26];
         
         for(int i = 0; i < s.length(); i++) {
-            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
+            dict[s.charAt(i)- 'a']++;
+            // hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
         }
         for(int i = 0; i < t.length(); i++) {
-            hm.put(t.charAt(i), hm.getOrDefault(t.charAt(i), 0) - 1);
+            dict[t.charAt(i)- 'a']--;
+            // hm.put(t.charAt(i), hm.getOrDefault(t.charAt(i), 0) - 1);
         }
         
-        for(char key: hm.keySet()) {
-            if(hm.get(key) != 0) return key;
+        for(int i = 0; i < 26; i++) {
+            if(dict[i] != 0) return (char) (i+ 'a');
         }
+        
+        // for(char key: hm.keySet()) {
+        //     if(hm.get(key) != 0) return key;
+        // }
         return '0';
     }
 }
