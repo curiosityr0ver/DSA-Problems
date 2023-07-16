539d37123cb8148b5795922beff79fca9b32995c
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-07-16 12:39:05 +0530

    Time: 18 ms (10.60%), Space: 74 MB (90.17%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -4,8 +4,6 @@ class Solution {
         ArrayList<Integer> pos = new ArrayList<>();
         ArrayList<Integer> neg = new ArrayList<>();
 
-        
-        
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] > 0) {
                 pos.add(nums[i]);
@@ -13,20 +11,16 @@ class Solution {
                 neg.add(nums[i]);
             }
         }
+        
         int ptr1 = 0, ptr2 = 0;
-        int[] res = new int[nums.length];
         
         for(int i = 0; i < nums.length; i++) {
             if(i%2 == 0) {
-                res[i] = pos.get(ptr1++);
+                nums[i] = pos.get(ptr1++);
             } else {
-                res[i] = neg.get(ptr2++);
+                nums[i] = neg.get(ptr2++);
             }
         }
-        
-        
-
-        
-        return res;
+        return nums;
     }
 }
