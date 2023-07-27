5529c310d2fedb308690f1fe536faff53ce59c6c
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-07-27 14:38:39 +0530

    Time: 10 ms (5.46%), Space: 40.3 MB (5.35%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.java
+++ 461-hamming-distance/461-hamming-distance.java
@@ -3,21 +3,17 @@ class Solution {
         StringBuilder binx = new StringBuilder(), biny = new StringBuilder();
         
         while(x > 0) {
-            binx.append(x%2);
+            binx.insert(0, x%2);
             x = x/2;
         }
 
         while(y > 0) {
-            biny.append(y%2);
+            biny.insert(0, y%2);
             y = y/2;
         }
         
         int len = Math.max(binx.length(), biny.length());
         
-        binx = binx.reverse();
-        biny = biny.reverse();
-        
-        System.out.println(binx + ", " + biny);
         while(binx.length() < len) binx = binx.insert(0, "0");
         while(biny.length() < len) biny = biny.insert(0, "0"); 
         System.out.println(binx + ", " + biny);
