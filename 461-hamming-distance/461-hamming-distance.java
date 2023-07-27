f46b31909767a0d6480727871990e4f6b9ec0903
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-07-27 14:37:13 +0530

    Time: 11 ms (5.46%), Space: 40.1 MB (5.35%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.java
+++ 461-hamming-distance/461-hamming-distance.java
@@ -1,22 +1,27 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        String binx = "", biny = "";
+        StringBuilder binx = new StringBuilder(), biny = new StringBuilder();
         
         while(x > 0) {
-            binx = x%2 + binx;
+            binx.append(x%2);
             x = x/2;
         }
 
         while(y > 0) {
-            biny = y%2 + biny;
+            biny.append(y%2);
             y = y/2;
         }
         
-
         int len = Math.max(binx.length(), biny.length());
-        while(binx.length() < len) binx = "0" + binx;
-        while(biny.length() < len) biny = "0" + biny;
         
+        binx = binx.reverse();
+        biny = biny.reverse();
+        
+        System.out.println(binx + ", " + biny);
+        while(binx.length() < len) binx = binx.insert(0, "0");
+        while(biny.length() < len) biny = biny.insert(0, "0"); 
+        System.out.println(binx + ", " + biny);
+
         int res = 0;
         
         for(int i = 0; i < len; i++) if(binx.charAt(i) != biny.charAt(i)) res++; 
