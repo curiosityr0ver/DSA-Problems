960dae44770f8686df9c7910cd6f9fd8e31d61fa
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-08-01 13:56:16 +0530

    Time: 1 ms (100.00%), Space: 45.3 MB (65.09%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,19 @@
+class Solution {
+     List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        generateCombinations(1, n, k, new ArrayList<Integer>());
+        return result;
+    }
+
+    private void generateCombinations(int start, int n, int k, List<Integer> combination) {
+        if (k == 0) {
+            result.add(new ArrayList<>(combination));
+            return;
+        }
+        for (int i = start; i <= n - k + 1; i++) {
+            combination.add(i);
+            generateCombinations(i + 1, n, k - 1, combination);
+            combination.remove(combination.size() - 1);
+        }
+    }
+}
