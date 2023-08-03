d8a59b0ec2b42a374b2ada2eadded87f30b75262
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-08-04 01:38:43 +0530

    Time: 1 ms (78.65%), Space: 43.6 MB (7.12%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,62 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+
+    public List<String> letterCombinations(String digits) {
+        if(digits.length() == 0) return res;
+        helper("", 0, digits);
+        
+        return res;
+    }
+    
+    void helper(String str, int index, String digits) {
+        if(index == digits.length()) res.add(str);
+        else {
+            char ch = digits.charAt(index);
+            index++;
+            switch(ch) {
+              case '2':
+                helper(str + "a", index, digits);
+                helper(str + "b", index, digits);
+                helper(str + "c", index, digits);
+                return;
+              case '3':
+                helper(str + "d", index, digits);
+                helper(str + "e", index, digits);
+                helper(str + "f", index, digits);
+                return;
+             case '4':
+                helper(str + "g", index, digits);
+                helper(str + "h", index, digits);
+                helper(str + "i", index, digits);
+                return; 
+            case '5':
+                helper(str + "j", index, digits);
+                helper(str + "k", index, digits);
+                helper(str + "l", index, digits);
+                return; 
+            case '6':
+                helper(str + "m", index, digits);
+                helper(str + "n", index, digits);
+                helper(str + "o", index, digits);
+                return; 
+            case '7':
+                helper(str + "p", index, digits);
+                helper(str + "q", index, digits);
+                helper(str + "r", index, digits);
+                helper(str + "s", index, digits);
+                return; 
+            case '8':
+                helper(str + "t", index, digits);
+                helper(str + "u", index, digits);
+                helper(str + "v", index, digits);
+                return; 
+            case '9':
+                helper(str + "w", index, digits);
+                helper(str + "x", index, digits);
+                helper(str + "y", index, digits);
+                helper(str + "z", index, digits);
+                return; 
+            }
+        }
+    }
+}
