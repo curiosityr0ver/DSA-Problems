f2125ccc2a241b0dfbb75964fb9ea9b929a8ac0d
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-08-07 19:26:59 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (96.14%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix2(int[][] matrix, int target) {
         int top = 0, bottom = matrix.length-1, mid;
         int [] temp;
         while(top <= bottom) {
@@ -20,6 +20,11 @@ class Solution {
             } else if(target < temp[0]) bottom = mid -1;
             else top = mid + 1;
         }
+        return false;
+    }
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int [] row: matrix) for(int ele: row) if(ele == target) return true;
         return false;
     }
 }
