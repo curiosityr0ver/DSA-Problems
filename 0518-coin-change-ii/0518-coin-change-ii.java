17bb571589e664ce3b41bebc1280651e61e620b9
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-08-11 11:20:21 +0530

    Time: 6 ms (67.34%), Space: 46.4 MB (8.79%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,35 +1,21 @@
 class Solution {
-    int [] coinsArr;
-    int[][] dp;
-    int res = 0;
-    
     public int change(int amount, int[] coins) {
-        coinsArr = coins;
-        dp = new int[coins.length+1][amount+1];
-        
-        for(int [] row: dp) {
-            Arrays.fill(row, -1);
-        }
-        
-        return dfs(0, amount);
-    }
-    
-    int dfs(int index, int amt) {
-        if(dp[index][amt] > -1) return dp[index][amt];
-        
-        
-        if(index == coinsArr.length) {
-            dp[index][amt] = (amt == 0) ? 1 : 0;
-            
-        } else {
-            int sum = 0;
-            while(amt >= sum) {
-                dp[index][amt] += dfs(index+1, amt-sum);
-                sum += coinsArr[index];
-            }
-            dp[index][amt]++; 
-        }
-        
-        return dp[index][amt];
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+       
+       for(int i = 0;i<=amount;i++) {
+           if(i % coins[0] == 0) dp[0][i] = 1;
+           
+       }
+
+       for(int i = 1;i<n;i++) {
+           for(int j = 0;j<=amount;j++) {
+               int dontPick = dp[i-1][j];
+               int pick = 0;
+               if(coins[i] <= j) pick = dp[i][j-coins[i]];
+               dp[i][j] = dontPick + pick;
+           }
+       }
+        return dp[n-1][amount];
     }
 }
