a70d7d9e41ecc570cb2cde026405d545a65ec9e5
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-08-13 03:16:10 +0530

    Time: 62 ms (5.49%), Space: 44.5 MB (5.49%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.java
+++ 0980-unique-paths-iii/0980-unique-paths-iii.java
@@ -1,7 +1,18 @@
 class Solution {
+    int [][] dp = new int[100][100];
+    
+    
+    
     int count = 1;
     public int uniquePathsIII(int[][] grid) {
         int x = 0, y = 0;
+        
+        for(int i = 0; i < 100; i++) {
+            for(int j = 0; j < 100; j++) {
+                dp[i][j] = -1;
+            }
+        } 
+        
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[0].length; j++) {
                 if(grid[i][j] == 0) count++;
@@ -16,20 +27,29 @@ class Solution {
     }
     
     int dfs(int x, int y, String vis, int [][] grid) {
-        if(x == grid.length || y == grid[0].length || x < 0 || y < 0 || grid[x][y] == -1) return 0;
 
-        if(grid[x][y] == 2) {
-            
-            return vis.split(" ").length == count ? 1 : 0;
-//             for(String v: visArr) System.out.print("(" + v.split(",")[0] + ", " + v.split(",")[1] + "), ");
-//             System.out.println();
+        if(x == grid.length || y == grid[0].length || x < 0 || y < 0 || grid[x][y] == -1) {
+            return 0;
         }
         
-            if(vis.length() > 0) for(String v: vis.split(" ")) if(v.equals(x + "," + y)) return 0;
-            
+        if(dp[x][y] >= 0) return dp[x][y];
+
+
+        if(grid[x][y] == 2) {
+            // for(String v: vis.split(" ")) System.out.print("(" + v + "),");
+            // System.out.println();
+            return vis.split(" ").length == count ? 1 : 0;
+            // return dp[x][y];
+        }
+        
+            if(vis.length() > 0) for(String v: vis.split(" ")) if(v.equals(x + "," + y))  {
+                // dp[x][y] = 0;
+                return 0;
+            }
 
             vis += x + "," + y + " ";
 
         return dfs(x, y+1, vis, grid) + dfs(x, y-1, vis, grid) + dfs(x+1, y, vis, grid) + dfs(x-1, y, vis, grid);
+        // return dp[x][y];
     }
 }
