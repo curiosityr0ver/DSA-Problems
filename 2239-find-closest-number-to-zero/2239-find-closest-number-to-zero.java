1d0d232e4b560fe4f06bf96e1a8f3b1f8ff2b92d
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-08-16 03:26:25 +0530

    Time: 3 ms (34.33%), Space: 44 MB (37.80%) - LeetHub

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.java
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.java
@@ -4,11 +4,8 @@ class Solution {
         int res = 999999;
         
         for(int ele: nums) {
-            if(Math.abs(ele) < Math.abs(res)) {
-                res = ele;
-            } else if(Math.abs(ele) == Math.abs(res)) {
-                res = Math.max(res, ele);
-            }
+            if(Math.abs(ele) < Math.abs(res)) res = ele;
+            else if(Math.abs(ele) == Math.abs(res)) res = Math.max(res, ele);
         }
         
         return res;
