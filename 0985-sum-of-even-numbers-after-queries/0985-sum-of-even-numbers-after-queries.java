c8b9ddf0e9964a72b8ebed15750b3e6b031c2993
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-08-17 00:58:10 +0530

    Time: 4 ms (99.16%), Space: 47.7 MB (33.47%) - LeetHub

--- 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.java
+++ 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.java
@@ -6,10 +6,12 @@ class Solution {
 
         
         for(int[] pair: queries) {
-            if(nums[pair[1]] % 2 == 0) {
-                if(pair[0]%2 == 0) sum += pair[0];
-                else sum -= nums[pair[1]];
-            } else if(pair[0]%2 != 0)sum += nums[pair[1]] + pair[0];
+            // if(nums[pair[1]] % 2 == 0) {
+            //     sum += pair[0]%2 == 0 ? pair[0] : -nums[pair[1]];
+                sum += nums[pair[1]] % 2 == 0 ? (pair[0]%2 == 0 ? pair[0] : -nums[pair[1]]) : (pair[0]%2 != 0) ?  nums[pair[1]] + pair[0] : 0;
+
+                
+            // } else if(pair[0]%2 != 0)sum += nums[pair[1]] + pair[0];
 
             nums[pair[1]] += pair[0];                      
             ans[j++] = sum;  
