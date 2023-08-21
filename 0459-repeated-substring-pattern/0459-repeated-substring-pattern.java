5ef39d2df03ddc778115db01e479342a7fb8896e
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-08-21 13:15:13 +0530

    Time: 1154 ms (5.01%), Space: 43.5 MB (98.89%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i = 1;i < s.length(); i++) {
             if(s.length() % i > 0) continue;
             
-            String temp = s.substring(0, i);
             boolean res = true;
             for(int j = i; j < s.length(); j+=i) {
                 if(!isEqual(s, i, j)) {
