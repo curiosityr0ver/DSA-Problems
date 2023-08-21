1910affdb53429e34fab366390ffb3c1e9b29dfc
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-08-21 13:13:11 +0530

    Time: 13 ms (82.94%), Space: 44.2 MB (46.38%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -22,8 +22,8 @@ class Solution {
         String s1 = s.substring(0, r1);
         String s2 = s.substring(l2, r1+l2);
         
-        for(int i = 0; i < s1.length(); i++) {
-            if(s1.charAt(i) != s2.charAt(i)) return false;
+        for(int i = 0; i < r1; i++) {
+            if(s.charAt(i) != s.charAt(i+l2)) return false;
         }
         
         return true;
