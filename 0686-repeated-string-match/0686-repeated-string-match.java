10a5e171b122f8abf1e89b5da0cc6a53ae201ea3
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-08-21 14:35:27 +0530

    Time: 1052 ms (5.20%), Space: 44.3 MB (14.34%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -4,15 +4,10 @@ class Solution {
         
         while(a.length() < b.length()) a += temp;
         
-//         for(int i = 1; i < 2; i++) {
-            
-//             if(isSubs(a,b)) return a.length()/temp.length();            
-//             a += temp;
-//         }
+
         if(isSubs(a,b)) return a.length()/temp.length();            
         a += temp;
         if(isSubs(a,b)) return a.length()/temp.length();            
-        // a += temp;
         
        return -1; 
     }
@@ -20,17 +15,13 @@ class Solution {
     boolean isSubs(String parent, String child) {
             
         for(int i = 0; i < parent.length() - child.length()+1; i++) {
-            if(parent.charAt(i) != child.charAt(0)) continue;
             if(equals(parent, child, i)) return true;
         }
         return false;
     }
     
     boolean equals(String s1, String s2,int exp) {
-        
         for(int i = 0; i < s2.length(); i++) if(s1.charAt(i+exp) != s2.charAt(i)) return false;
-        
-        
         return true;
     }
 }
