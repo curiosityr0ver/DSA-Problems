93f6334e70343b3143da5b063820fa2fde72bec2
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-08-29 07:59:57 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (98.13%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,11 +1,13 @@
 class MyStack {
         Queue<Integer> q1, q2;
-        boolean curr = false;
+        boolean curr;
+        int res;
     
 
     public MyStack() {
         q1 = new LinkedList<Integer>();
         q2 = new LinkedList<Integer>();
+        curr = false;
         
     }
     
@@ -15,7 +17,7 @@ class MyStack {
     }
     
     public int pop() {
-        if(curr) {
+        if(q2.isEmpty()) {
             while(q1.size() > 1) q2.add(q1.remove());
             curr = !curr;
             return q1.remove();
@@ -24,28 +26,24 @@ class MyStack {
             curr = !curr;
             return q2.remove();
         }
-        
     }
     
     public int top() {
         
         if(curr) {
             while(q1.size() > 1) q2.add(q1.remove());
-            int res = q1.remove();
+            res = q1.remove();
             q2.add(res);
-            curr = !curr;
-            return res;
         } else {
             while(q2.size() > 1) q1.add(q2.remove());
-            int res = q2.remove();
+            res = q2.remove();
             q1.add(res);
-            curr = !curr;
-            return res;
         }
+        curr = !curr;
+        return res;
     }
     
     public boolean empty() {
-        
         return q1.size() == 0 && q2.size() == 0;
     }
 }
