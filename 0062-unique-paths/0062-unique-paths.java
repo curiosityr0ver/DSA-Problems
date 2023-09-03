2dd111f652f9fbc373ea5ff4c2452b4022ada67c
Ishu Mehta <62722431+curiosityr0ver@users.noreply.github.com>
2023-09-03 15:41:39 +0530

    Time: 1 ms (8.81%), Space: 39.5 MB (34.46%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,13 @@
+class Solution {  
+    public int uniquePaths(int m, int n) {
+        Integer[][] map = new Integer[m][n];
+
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(i == 0 || j == 0) map[i][j] = 1;
+                else map[i][j] = map[i-1][j]+map[i][j-1];        
+            }
+        }
+        return map[m-1][n-1];   
+    }
+}
